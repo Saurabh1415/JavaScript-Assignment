d0aa881a9f8af3e08f7398896f0e090087ef75ba
Saurabh Singh <saurabh.ss1415@gmail.com>
2022-05-06 18:14:04 +0000

    new program

--- Problem10.js
+++ Problem10.js
@@ -1,5 +1,25 @@
-function Loki(){
-  for(let i=0;i>=n;i++){
-    
+function Loki(n,arr){
+  let evenVal = [];
+  for(let i=0;i<n;i++){
+    if(i%2==0)
+    {
+      evenVal.push(arr[i]);
+    }
+    else{
+      continue;
+    }
   }
+  evenVal.sort((a,b)=> a-b);
+  for(let i=0;i<n;i++)
+  {
+    if(i%2==0)
+    {
+      arr[i] = evenVal.shift();
+    }
+    else{
+      continue;
+    }
+  }
+  return arr; 
 }
+console.log(Loki(5,[3, 9, 1, 44, 6]));
